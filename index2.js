1dc42814a6ede8870ee9e1dd75faffce9f0f3197
Ocean756 <mennatallamohamed756@gmail.com>
2025-07-21 02:28:11 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,12 @@
+let name1 = prompt("what is your name?");
+let name2 = prompt("what is your name?");
+let name3 = prompt("what is your name?");
+
+
+console.log("attended:");
+console.log("1. " + name1);
+console.log("2. " + name2);
+console.log("3. " + name3);
+
+
+console.log("attended: 3");
